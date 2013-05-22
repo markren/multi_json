9597bd25b6b0c99bd683d88681aab8923a449f14
Mark Rentz <mark@trover.com>
2013-05-22 11:49:31 -0700

    Prevent error for undefined Yajl::ParseError

--- lib/multi_json/adapters/yajl.rb
+++ lib/multi_json/adapters/yajl.rb
@@ -5,7 +5,11 @@ module MultiJson
   module Adapters
     # Use the Yajl-Ruby library to dump/load.
     class Yajl < Adapter
-      ParseError = ::Yajl::ParseError
+      ParseError = if defined?(::Yajl::ParseError)
+        ::Yajl::ParseError
+      else
+        SyntaxError
+      end
 
       def load(string, options={})
         ::Yajl::Parser.new(:symbolize_keys => options[:symbolize_keys]).parse(string)
